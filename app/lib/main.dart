422bbb88924d0d8abde316c29a2e3469de5b9c8d
Justin Phu <justinqphu@gmail.com>
2020-07-26 17:24:55 -0700

    [app] Add removing and adding new habits
    This introduces the ability to add / remove new habits. This doesn't
    communicate with the database. The database still lazily creates habits
    and never removes them.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -54,10 +54,10 @@ class HabitsState extends State<Habits> {
   final _completed = Set<String>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
-  Widget _buildSuggestions() {
+  Widget _buildSuggestions(BuildContext context) {
     return ReorderableListView(
         children: List.generate(_habits.length, (index) {
-          return _buildRow(_habits[index]);
+          return _buildRow(context, index);
         }),
         onReorder: (oldIndex, newIndex) {
           setState(() {
@@ -71,37 +71,89 @@ class HabitsState extends State<Habits> {
         });
   }
 
-  Widget _buildRow(String habit) {
+  Widget _buildRow(BuildContext context, int index) {
+    String habit = _habits[index];
     final isCompleted = _completed.contains(habit);
-    return ListTile(
+    return Dismissible(
+        // Duplicate the key since we don't care if this is a dismissable widget or a list tile wideget.
         key: ValueKey(habit),
-        title: Text(
-          habit,
-          style: _biggerFont,
-        ),
-        trailing: Icon(
-          isCompleted ? Icons.done : Icons.done_outline,
-          color: isCompleted ? Colors.red : null,
-        ),
-        onTap: () {
+        onDismissed: (direction) {
+          // Remove item from the list
           setState(() {
-            if (isCompleted) {
-              _completed.remove(habit);
-              habitHttpRequest(habit, false);
-            } else {
-              _completed.add(habit);
-              // TODO: Don't ignore the result.
-              habitHttpRequest(habit, true);
-            }
+            _habits.removeAt(index);
           });
-        });
+
+          // Show a snackbar. This snackbar could also contain "Undo" actions.
+          Scaffold.of(context)
+              .showSnackBar(SnackBar(content: Text("$habit dismissed")));
+        },
+        background: Container(color: Colors.red),
+        child: ListTile(
+            key: ValueKey(habit),
+            title: Text(
+              habit,
+              style: _biggerFont,
+            ),
+            trailing: Icon(
+              isCompleted ? Icons.done : Icons.done_outline,
+              color: isCompleted ? Colors.red : null,
+            ),
+            onTap: () {
+              setState(() {
+                if (isCompleted) {
+                  _completed.remove(habit);
+                  habitHttpRequest(habit, false);
+                } else {
+                  _completed.add(habit);
+                  // TODO: Don't ignore the result.
+                  habitHttpRequest(habit, true);
+                }
+              });
+            }));
+  }
+
+  // Add a habit to the list and update the state.
+  void _addHabitItem(String habit_name) {
+    logger.d("Add habit with name: ${habit_name}");
+    // Only add the task if the user actually entered something
+    if (habit_name.length > 0) {
+      setState(() => _habits.insert(_habits.length, habit_name));
+    }
+  }
+
+  // Push the add habit button. This will bring up a navigation menu and allow the user to add a habit.
+  void _pushAddHabitScreen() {
+    // Push this page onto the stack
+    Navigator.of(context).push(
+        // MaterialPageRoute will automatically animate the screen entry, as well
+        // as adding a back button to close it
+        new MaterialPageRoute(builder: (context) {
+      return new Scaffold(
+          appBar: new AppBar(title: new Text('Add a new habit')),
+          body: new TextField(
+            autofocus: true,
+            onSubmitted: (val) {
+              _addHabitItem(val);
+              Navigator.pop(context); // Close the add todo screen
+            },
+            decoration: new InputDecoration(
+                hintText: 'Enter something to do...',
+                contentPadding: const EdgeInsets.all(16.0)),
+          ));
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Mindless')),
-      body: _buildSuggestions(),
+      body: Builder(builder: (BuildContext context) {
+        return _buildSuggestions(context);
+      }),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: _pushAddHabitScreen,
+          tooltip: 'Add a habit',
+          child: new Icon(Icons.add)),
     );
   }
 }
