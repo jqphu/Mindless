a513f84a214aed88de7294b53cdf326a1dccaa9b
Justin Phu <justinqphu@gmail.com>
2020-07-27 19:29:20 -0700

    [app] Add a Facebook login page
    This allows us to login using facebook and not store any secrets
    ourselves. We still need to extract the email + hash it and use it as a unique
    identifier for the database.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart' as foundation;
 import 'package:logger/logger.dart';
 import 'dart:convert';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 var logger = Logger(printer: PrettyPrinter());
 
@@ -11,10 +12,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Startup Name Generator',
-      home: Habits(),
-    );
+    return MaterialApp(initialRoute: '/login', routes: {
+      '/login': (context) => LoginPage(),
+      '/habits': (context) => Habits()
+    });
   }
 }
 
@@ -37,6 +38,7 @@ void habitHttpRequest(String habit, bool markHabit) async {
 
   final response = await http.post(habitEndpoint,
       body: jsonEncode({
+        "username": "dummy",
         "name": habit,
         "should_mark": markHabit,
       }));
@@ -168,4 +170,118 @@ class HabitsState extends State<Habits> {
 class Habits extends StatefulWidget {
   @override
   HabitsState createState() => new HabitsState();
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isLoggedIn = false;
+  var profileData;
+
+  var facebookLogin = FacebookLogin();
+
+  void onLoginStatusChanged(bool isLoggedIn, {profileData}) {
+    if (isLoggedIn) {
+      // On login, move to habits route.
+      Navigator.pushNamed(context, '/habits');
+    }
+    setState(() {
+      this.isLoggedIn = isLoggedIn;
+      this.profileData = profileData;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text("Facebook Login"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Colors.white,
+              ),
+              onPressed: () => facebookLogin.isLoggedIn
+                  .then((isLoggedIn) => isLoggedIn ? _logout() : {}),
+            ),
+          ],
+        ),
+        body: Container(
+          child: Center(
+            child: isLoggedIn
+                ? _displayUserData(profileData)
+                : _displayLoginButton(),
+          ),
+        ));
+  }
+
+  void initiateFacebookLogin() async {
+    var facebookLoginResult =
+        await facebookLogin.logInWithReadPermissions(['email']);
+
+    switch (facebookLoginResult.status) {
+      case FacebookLoginStatus.error:
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.loggedIn:
+        var graphResponse = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email,picture.width(400)&access_token=${facebookLoginResult.accessToken.token}');
+
+        var profile = json.decode(graphResponse.body);
+
+        onLoginStatusChanged(true, profileData: profile);
+        break;
+    }
+  }
+
+  _displayUserData(profileData) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          height: 200.0,
+          width: 200.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                profileData['picture']['data']['url'],
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 28.0),
+        Text(
+          "${profileData['name']}",
+          style: TextStyle(
+            fontSize: 20.0,
+            letterSpacing: 1.1,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  _displayLoginButton() {
+    return RaisedButton(
+      child: Text("Login with Facebook"),
+      onPressed: () => initiateFacebookLogin(),
+      color: Colors.blue,
+      textColor: Colors.white,
+    );
+  }
+
+  _logout() async {
+    await facebookLogin.logOut();
+    onLoginStatusChanged(false);
+  }
 }
