c082039ab94148d55d6564f2f811643892ef8955
Justin Phu <justinqphu@gmail.com>
2020-10-16 20:15:36 -0700

    [app][login] Add app bar header for registration page

--- app/lib/login/registration.dart
+++ app/lib/login/registration.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-/// The registration logic.
 class RegistrationPage extends StatefulWidget {
   @override
   _RegistrationPageState createState() => _RegistrationPageState();
@@ -11,7 +10,21 @@ class _RegistrationPageState extends State<RegistrationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildMonkeyBar(context),
       body: SafeArea(child: Text("Hello world!")),
     );
   }
+}
+
+/// Build the app bar with a monkey and a title.
+AppBar buildMonkeyBar(BuildContext context) {
+  return AppBar(
+    titleSpacing: 0.0,
+    title: Row(children: <Widget>[
+      Image.asset('assets/monkey.png', height: 40),
+      SizedBox(width: 11),
+      Text('MINDLESS',
+          style: Theme.of(context).textTheme.headline3, textScaleFactor: 0.75),
+    ]),
+  );
 }
