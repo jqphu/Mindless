8c3cdbbc3b7d77267d694dd5a4ee9a7ad9f76c30
Justin Phu <justinqphu@gmail.com>
2020-10-18 14:47:52 -0700

    [server][scripts] Update old comment that user exists
    We actually generate the user now so this comment is obsolete.

--- server/api/scripts/generate_json_request.py
+++ server/api/scripts/generate_json_request.py
@@ -120,7 +120,6 @@ def main(args):
         endpoint = 'http://127.0.0.1/mindless/api'
 
     create_users(endpoint)
-    # Assume user always exists use id 1.
     create_tasks(endpoint)
 
 if __name__ == '__main__':
