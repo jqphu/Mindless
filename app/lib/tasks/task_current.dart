f7c4f1c0bd73a10346a1cba9a8d63acbabd0498d
Justin Phu <justinqphu@gmail.com>
2020-11-22 06:25:35 -0800

    [app] Modify the task tab to remove the time

--- app/lib/tasks/task_current.dart
+++ app/lib/tasks/task_current.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:mindless/model/app_state.dart';
 
+import 'package:mindless/model/task.dart';
+
 class TaskCurrent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,11 +14,53 @@ class TaskCurrent extends StatelessWidget {
         }
 
         return Container(
-            height: MediaQuery.of(context).size.height / 15,
-            child: Center(
-                child: Text(model.currentTask.name,
-                    style: TextStyle(fontSize: 30))));
+          height: MediaQuery.of(context).size.height / 15,
+          color: Colors.lightBlue,
+          child: Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+                right: 8,
+              ),
+              child: TaskTextAndTime(task: model.currentTask)),
+        );
       },
+    );
+  }
+}
+
+class TaskTextAndTime extends StatelessWidget {
+  final Task task;
+  TaskTextAndTime({@required this.task}) : assert(task != null);
+
+  String _printDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    var twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    var twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return '${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Text(
+          task.name,
+          style: TextStyle(
+            color: Color.fromRGBO(0, 0, 0, 0.8),
+            fontSize: 18,
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        SizedBox(width: 50),
+        Text(
+          '${_printDuration(task.totalTimeSpentToday)}',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w300,
+          ),
+        )
+      ],
     );
   }
 }
--- app/lib/tasks/task_row_item.dart
+++ app/lib/tasks/task_row_item.dart
@@ -21,36 +21,40 @@ class TaskRowItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(builder: (context, model, child) {
       final row = SafeArea(
-          top: false,
-          bottom: false,
-          minimum: const EdgeInsets.only(
-            left: 16,
-            top: 8,
-            bottom: 8,
-            right: 8,
-          ),
-          child: Dismissible(
-            key: Key(task.name),
-            background: Container(color: Colors.redAccent),
-            onDismissed: (direction) {
-              // TODO: Removing tasks.
-            },
-            child: Row(
-              children: <Widget>[
-                Expanded(child: TaskTextAndTime(task: task)),
-                CupertinoButton(
-                  padding: EdgeInsets.zero,
-                  onPressed: () {
-                    // TODO: Starting tasks
-                  },
-                  child: Icon(
-                    CupertinoIcons.play_arrow,
-                    semanticLabel: 'Start',
-                  ),
-                ),
-              ],
+        top: false,
+        bottom: false,
+        minimum: const EdgeInsets.only(
+          left: 16,
+          top: 8,
+          bottom: 8,
+          right: 8,
+        ),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                child: Text(
+              task.name,
+              style: TextStyle(
+                color: Color.fromRGBO(0, 0, 0, 0.8),
+                fontSize: 20,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.normal,
+              ),
+            )),
+            MaterialButton(
+              minWidth: 0,
+              onPressed: () {
+                // TODO: Starting tasks
+              },
+              child: Icon(
+                Icons.more_vert,
+                color: Colors.grey,
+                semanticLabel: 'Options',
+              ),
             ),
-          ));
+          ],
+        ),
+      );
 
       // Just return the row without the bottom line if it is the last item.
       if (isLastItem) {
@@ -70,45 +74,5 @@ class TaskRowItem extends StatelessWidget {
         ],
       );
     });
-  }
-}
-
-class TaskTextAndTime extends StatelessWidget {
-  final Task task;
-  TaskTextAndTime({@required this.task}) : assert(task != null);
-
-  String _printDuration(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    var twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
-    var twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
-    return '${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds';
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          task.name,
-          style: TextStyle(
-            color: Color.fromRGBO(0, 0, 0, 0.8),
-            fontSize: 18,
-            fontStyle: FontStyle.normal,
-            fontWeight: FontWeight.normal,
-          ),
-        ),
-        const Padding(padding: EdgeInsets.only(top: 8)),
-        Text(
-          '${_printDuration(task.totalTimeSpentToday)}',
-          style: TextStyle(
-            color: Color(0xFF8E8E93),
-            fontSize: 13,
-            fontWeight: FontWeight.w300,
-          ),
-        )
-      ],
-    );
   }
 }
