9cd994c298fe81816acd1be107280ee2b54f0226
Justin Phu <justinqphu@gmail.com>
2021-07-23 16:51:50 -0700

    [app][task] Add switching back and forth from editing task name

--- app/lib/tasks/task_row_item.dart
+++ app/lib/tasks/task_row_item.dart
@@ -8,9 +8,7 @@ import 'package:mindless/model/task.dart';
 
 final log = Logger('TaskRowItem');
 
-class TaskRowItem extends StatelessWidget {
-  static const double rightOffset = 8;
-
+class TaskRowItem extends StatefulWidget {
   /// The task to display.
   final Task task;
 
@@ -23,8 +21,129 @@ class TaskRowItem extends StatelessWidget {
       : assert(task != null);
 
   @override
+  _TaskRowItemState createState() {
+    return _TaskRowItemState();
+  }
+}
+
+class _TaskRowItemState extends State<TaskRowItem> {
+  static const double rightOffset = 8;
+
+  /// Whether or not we are editing this task.
+  bool isEditing = false;
+
+  @override
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(builder: (context, model, child) {
+      var rowWidget;
+      if (!isEditing) {
+        rowWidget = InkWell(
+            child: Row(children: <Widget>[
+              Expanded(
+                  child: Text(
+                widget.task.name,
+                style: TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, 0.8),
+                  fontSize: 20,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.normal,
+                ),
+              )),
+              MaterialButton(
+                minWidth: 0,
+                onPressed: () async {
+                  // Get the Box which is the row.
+                  final RenderBox renderBox = context.findRenderObject();
+
+                  // Position of the row itself.
+                  final position = renderBox.localToGlobal(Offset.zero);
+
+                  // Size of the row.
+                  final size = renderBox.size;
+                  await showMenu(
+                      context: context,
+
+                      // We use the distance from top to be the position of
+                      // the row plus an offset. We use 3/4 of the height so
+                      // it starts within the row (easy to tell which item
+                      // we're modifying but near the bottom.)
+                      //
+                      // We use the full width as the size to push it to the
+                      // right but use the edge inset from right to match the
+                      // inset above.
+                      position: RelativeRect.fromLTRB(size.width,
+                          position.dy + (size.height * 3) / 4, rightOffset, 0),
+                      items: [
+                        PopupMenuItem<TaskRowOptionTypes>(
+                            value: TaskRowOptionTypes.delete,
+                            child: Row(
+                              children: [
+                                Icon(Icons.delete, semanticLabel: 'Delete'),
+                                SizedBox(width: 10),
+                                Text('Delete')
+                              ],
+                            )),
+                        PopupMenuItem<TaskRowOptionTypes>(
+                            value: TaskRowOptionTypes.editName,
+                            child: Row(
+                              children: [
+                                Icon(Icons.edit),
+                                SizedBox(width: 10),
+                                Text('Edit name'),
+                              ],
+                            ))
+                      ]).then((taskRowOption) {
+                    switch (taskRowOption) {
+                      case TaskRowOptionTypes.delete:
+                        log.info('Delete task');
+                        model.deleteTask(widget.task);
+                        break;
+                      case TaskRowOptionTypes.editName:
+                        log.info('Edit name');
+                        isEditing = true;
+                        break;
+                      default:
+                        log.info('Nothing selected.');
+                    }
+                  });
+                },
+                child: Icon(
+                  Icons.more_vert,
+                  color: Colors.grey,
+                  semanticLabel: 'Options',
+                ),
+              ),
+            ]),
+            onTap: () => model.currentTask = widget.task);
+      } else {
+        rowWidget = Row(children: <Widget>[
+          Expanded(
+              child: Text(
+            'EDITING',
+            style: TextStyle(
+              color: Color.fromRGBO(0, 0, 0, 0.8),
+              fontSize: 20,
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.normal,
+            ),
+          )),
+          MaterialButton(
+            minWidth: 0,
+            onPressed: () {
+              log.info('Save button pressed for name edit.');
+              setState(() {
+                isEditing = false;
+              });
+            },
+            child: Icon(
+              Icons.check_box,
+              color: Colors.blue,
+              semanticLabel: 'Save',
+            ),
+          ),
+        ]);
+      }
+
       final row = SafeArea(
           top: false,
           bottom: false,
@@ -34,88 +153,10 @@ class TaskRowItem extends StatelessWidget {
             bottom: 8,
             right: rightOffset,
           ),
-          child: InkWell(
-              child: Row(children: <Widget>[
-                Expanded(
-                    child: Text(
-                  task.name,
-                  style: TextStyle(
-                    color: Color.fromRGBO(0, 0, 0, 0.8),
-                    fontSize: 20,
-                    fontStyle: FontStyle.normal,
-                    fontWeight: FontWeight.normal,
-                  ),
-                )),
-                MaterialButton(
-                  minWidth: 0,
-                  onPressed: () async {
-                    // Get the Box which is the row.
-                    final RenderBox renderBox = context.findRenderObject();
-
-                    // Position of the row itself.
-                    final position = renderBox.localToGlobal(Offset.zero);
-
-                    // Size of the row.
-                    final size = renderBox.size;
-                    await showMenu(
-                        context: context,
-
-                        // We use the distance from top to be the position of
-                        // the row plus an offset. We use 3/4 of the height so
-                        // it starts within the row (easy to tell which item
-                        // we're modifying but near the bottom.)
-                        //
-                        // We use the full width as the size to push it to the
-                        // right but use the edge inset from right to match the
-                        // inset above.
-                        position: RelativeRect.fromLTRB(
-                            size.width,
-                            position.dy + (size.height * 3) / 4,
-                            rightOffset,
-                            0),
-                        items: [
-                          PopupMenuItem<TaskRowOptionTypes>(
-                              value: TaskRowOptionTypes.delete,
-                              child: Row(
-                                children: [
-                                  Icon(Icons.delete, semanticLabel: 'Delete'),
-                                  SizedBox(width: 10),
-                                  Text('Delete')
-                                ],
-                              )),
-                          PopupMenuItem<TaskRowOptionTypes>(
-                              value: TaskRowOptionTypes.editName,
-                              child: Row(
-                                children: [
-                                  Icon(Icons.edit),
-                                  SizedBox(width: 10),
-                                  Text('Edit name'),
-                                ],
-                              ))
-                        ]).then((taskRowOption) {
-                      switch (taskRowOption) {
-                        case TaskRowOptionTypes.delete:
-                          model.deleteTask(task);
-                          break;
-                        case TaskRowOptionTypes.editName:
-                          log.info('Edit request');
-                          break;
-                        default:
-                          log.info('Nothing selected.');
-                      }
-                    });
-                  },
-                  child: Icon(
-                    Icons.more_vert,
-                    color: Colors.grey,
-                    semanticLabel: 'Options',
-                  ),
-                ),
-              ]),
-              onTap: () => model.currentTask = task));
+          child: rowWidget);
 
       // Just return the row without the bottom line if it is the last item.
-      if (isLastItem) {
+      if (widget.isLastItem) {
         return row;
       }
 
